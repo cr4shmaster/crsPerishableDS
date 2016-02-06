ab62376323e158546694c328f6026e1888ab8ad4
Tony Kalou <cr4shmaster@hotmail.com>
2016-02-06 15:56:13 +0200

    SW update

--- modinfo.lua
+++ modinfo.lua
@@ -1,10 +1,16 @@
-name = "Perishable"
+name = "Perishable TEST"
 description = "This mod is required by other mods that are using 'perishable.lua'."
 author = "cr4shmaster"
-version = "2.0.4"
+version = "2.1.0"
 forumthread = ""
 api_version = 6
 dont_starve_compatible = true
 reign_of_giants_compatible = true
+shipwrecked_compatible = true
 icon_atlas = "modicon.xml"
 icon = "modicon.tex"
+
+--[[ 
+Notes:
+ - none
+--]]
